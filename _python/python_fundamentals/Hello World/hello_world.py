f4e30b6513a40217bab555d491c6a38ad17db20f
sahar_murrar <murrarsahar@gmail.com>
2021-05-07 00:06:38 +0300

    Hello World Assignment/ python stack

---
+++ _python/python_fundamentals/Hello World/hello_world.py
@@ -0,0 +1,11 @@
+#print("Hello World!")
+myname= "Sahar Murrar"
+#print("Hello {{",myname, "}}!")
+#print("Hello {{"+myname+"}}!")
+num=4
+#print("Hello {{", num,"}}!")
+#print("Hello {{"+ str(num)+"}}!")
+food1="vegtabels"
+food2="soap"
+#print("I love to eat {{"+food1+"}}"+ " and "+"{{"+food2+"}}")
+print(f"I love to eat {food1} and {food2}")
