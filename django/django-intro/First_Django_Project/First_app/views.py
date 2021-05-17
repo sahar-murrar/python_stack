61e79f6519af98da75194c3662339467f9614309
sahar_murrar <murrarsahar@gmail.com>
2021-05-17 14:41:27 +0300

    This is My First Django Project/Python

--- django/django-intro/First_Django_Project/First_app/views.py
+++ django/django-intro/First_Django_Project/First_app/views.py
@@ -11,7 +11,7 @@ def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 
 def create(request):
-    return redirect('')
+    return redirect('/') #you have to put the / in the redirect, it will not work if yu put ''
 
 def show(request,number):
     return HttpResponse(f"placeholder to display blog number: {number}")
